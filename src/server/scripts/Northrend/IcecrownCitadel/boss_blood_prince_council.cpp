d92d59955935ecaa57b76a5e178f1a4ff7d0786e
Kimbanjang <dhejrgn@gmail.com>
2012-05-11 07:03:12 +0900

    피의회 복구 및 라나텔 리셋 수정

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
@@ -251,8 +251,6 @@ class boss_blood_council_controller : public CreatureScript
 
             void JustReachedHome()
             {
-                instance->SetBossState(DATA_BLOOD_PRINCE_COUNCIL, FAIL);
-                instance->SetData(DATA_BLOOD_PRINCE_COUNCIL, FAIL);
                 _resetCounter = 0;
                 if (Creature* keleseth = ObjectAccessor::GetCreature(*me, instance->GetData64(DATA_PRINCE_KELESETH_GUID)))
                     keleseth->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
@@ -459,20 +457,12 @@ class boss_prince_keleseth_icc : public CreatureScript
             {
                 summons.Summon(summon);
                 Position pos;
-                pos.Relocate(summon);
+                me->GetPosition(&pos);
                 float maxRange = me->GetDistance2d(summon);
                 float angle = me->GetAngle(summon);
-                // prevent spawning outside of room
-                while (!me->IsWithinLOS(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ()))
-                {
-                    maxRange -= 5.0f;
-                    if (maxRange < 5.0f)
-                        break;
-
-                    summon->MovePosition(pos, float(rand_norm() * maxRange), angle);
-                }
-
+                me->MovePositionToFirstCollision(pos, maxRange, angle);
                 summon->NearTeleportTo(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), pos.GetOrientation());
+                summon->ToTempSummon()->SetTempSummonType(TEMPSUMMON_CORPSE_DESPAWN);
             }
 
             void DamageDealt(Unit* /*target*/, uint32& damage, DamageEffectType damageType)
@@ -1253,8 +1243,8 @@ class npc_kinetic_bomb : public CreatureScript
                 me->CastSpell(me, SPELL_UNSTABLE, true);
                 me->CastSpell(me, SPELL_KINETIC_BOMB_VISUAL, true);
                 me->SetReactState(REACT_PASSIVE);
-                me->SetSpeed(MOVE_FLIGHT, IsHeroic() ? 0.3f : 0.15f, true);
                 me->GetPosition(_x, _y, _groundZ);
+                me->DespawnOrUnsummon(60000);
                 _groundZ = me->GetMap()->GetHeight(me->GetPhaseMask(), _x, _y, _groundZ, true, 500.0f);
             }
 
@@ -1280,10 +1270,10 @@ class npc_kinetic_bomb : public CreatureScript
                     {
                         case EVENT_BOMB_DESPAWN:
                             me->SetVisible(false);
+                            me->DespawnOrUnsummon(5000);
                             break;
                         case EVENT_CONTINUE_FALLING:
-                            me->GetMotionMaster()->Clear();
-                            me->GetMotionMaster()->MovePoint(POINT_KINETIC_BOMB_IMPACT, _x, _y, _groundZ);
+                            me->GetMotionMaster()->MoveCharge(_x, _y, _groundZ, me->GetSpeed(MOVE_WALK), POINT_KINETIC_BOMB_IMPACT);
                             break;
                         default:
                             break;
@@ -1346,17 +1336,16 @@ class npc_dark_nucleus : public CreatureScript
 
             void DamageTaken(Unit* attacker, uint32& /*damage*/)
             {
-                if (attacker == me)
+                if (attacker == me || attacker == me->getVictim())
                     return;
 
-                if (!_lockedTarget)
-                    if (me->getVictim() == attacker)
-                        _lockedTarget = true;
+                me->DeleteThreatList();
+                me->AddThreat(attacker, 500000000.0f);
             }
 
             void UpdateAI(uint32 const diff)
             {
-                if (!me->isInCombat())
+                if (!UpdateVictim())
                     return;
 
                 if (_targetAuraCheck <= diff)
@@ -1372,23 +1361,6 @@ class npc_dark_nucleus : public CreatureScript
                 }
                 else
                     _targetAuraCheck -= diff;
-
-                if (!_lockedTarget)
-                {
-                    if (Unit* victim = me->SelectVictim())
-                    {
-                        if (me->getVictim() && me->getVictim() != victim)
-                        {
-                            me->getVictim()->RemoveAurasDueToSpell(SPELL_SHADOW_RESONANCE_RESIST, me->GetGUID());
-                            _lockedTarget = true;
-                        }
-
-                        _lockedTarget = true;
-                        AttackStart(victim);
-                        DoCast(victim, SPELL_SHADOW_RESONANCE_RESIST);
-                        me->ClearUnitState(UNIT_STATE_CASTING);
-                    }
-                }
             }
 
         private:
@@ -1604,7 +1576,7 @@ class spell_valanar_kinetic_bomb_knockback : public SpellScriptLoader
 
             void Register()
             {
-                AfterHit += SpellHitFn(spell_valanar_kinetic_bomb_knockback_SpellScript::KnockIntoAir);
+                BeforeHit += SpellHitFn(spell_valanar_kinetic_bomb_knockback_SpellScript::KnockIntoAir);
             }
         };
 
