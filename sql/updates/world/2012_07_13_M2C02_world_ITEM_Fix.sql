b9f443b3bb69dc7e71ca1ef845964ea46a45e125
Kimbanjang <dhejrgn@gmail.com>
2012-08-20 09:19:31 +0900

    최초세팅 수정 : 이후 한번더 정리하여  기존 SQL 수정하고 생성으로 나감

--- sql/updates/world/2012_07_13_M2C02_world_ITEM_Fix.sql
+++ sql/updates/world/2012_07_13_M2C02_world_ITEM_Fix.sql
@@ -49,11 +49,11 @@ UPDATE `item_template` SET `dmg_min1` = FLOOR((`dmg_min1` + `dmg_max1`) / 2), `d
 -- 누락된 냉혹템 추가
 DELETE FROM `npc_vendor` WHERE `entry` IN (34060, 34078) AND `item` IN (42133, 42134, 42135, 42136, 42137);
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES
--- HORDE
+-- 호드
 (34060, 0, 42137, 0, 0, 2718),(34060, 0, 42136, 0, 0, 2718),(34060, 0, 42134, 0, 0, 2718),(34060, 0, 42135, 0, 0, 2718),(34060, 0, 42133, 0, 0, 2718),
--- ALLIANCE
+-- 얼라이언스
 (34078, 0, 42137, 0, 0, 2718),(34078, 0, 42136, 0, 0, 2718),(34078, 0, 42134, 0, 0, 2718),(34078, 0, 42135, 0, 0, 2718),(34078, 0, 42133, 0, 0, 2718);
--- SHIELDS
+-- 방패
 DELETE FROM `npc_vendor` WHERE `entry` = 33927 AND `item` IN (42561, 42566, 42572);
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES
 (33927, 0, 42561, 0, 0, 2701),(33927, 0, 42566, 0, 0, 2701),(33927, 0, 42572, 0, 0, 2701);
