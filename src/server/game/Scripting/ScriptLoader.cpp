fbce13f07eb5190a321d8d735a3334d5216e3cff
arin <arindpfmsls@gmail.com>
2012-09-08 23:50:56 +0900

    스크립트 로더에 커스텀 스크립트 추가

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -24,6 +24,14 @@ void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
 
+//custom
+void AddSC_Sweet_TeleportNPC();
+void AddSC_Lyn_change();
+void AddSC_npcs_mini_boss();
+void AddSC_npc_lottery();
+
+
+
 // spells
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
@@ -1272,6 +1280,11 @@ void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
+	AddSC_Sweet_TeleportNPC();
+	AddSC_Lyn_change();
+	AddSC_npcs_mini_boss();
+	AddSC_npc_lottery();
+
 
 #endif
 }
