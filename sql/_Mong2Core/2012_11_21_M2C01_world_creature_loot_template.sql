bd436e0d603bb669f8aa0e6db37eeee34c06329a
Kimbanjang <dhejrgn@gmail.com>
2012-12-03 10:54:33 +0900

    fetch 를 위한 정리

--- sql/_Mong2Core/2012_11_21_M2C01_world_creature_loot_template.sql
+++
@@ -1,9 +0,0 @@
--- 교수 비행선 아이템 드랍 (100%-1 / 75%-2 / 100%-1 / 75%-2)
-DELETE FROM `creature_loot_template` WHERE `mincountOrRef` IN (-91002,-91003,-91004,-91005);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
-(36678,2,100,1,0,-91002,1),(38431,2,75,1,0,-91003,2),(38585,3,100,1,0,-91004,1),(38586,2,75,1,0,-91005,2);
-
--- 싸울팽 비행선 아이템 드랍 (75%-1 / 50%-2 / 75%-1 / 50%-2)
-DELETE FROM `gameobject_loot_template` WHERE `mincountOrRef` IN (-91002,-91003,-91004,-91005);
-INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
-(28046,2,75,1,0,-91002,1),(28074,3,50,1,0,-91003,2),(28058,3,75,1,0,-91004,1),(28088,3,50,1,0,-91005,2);
