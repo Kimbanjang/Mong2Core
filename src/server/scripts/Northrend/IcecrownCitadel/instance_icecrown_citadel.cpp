d252cfaab5f00e53da0a1f76ba656c508e36bb05
Kimbanjang <dhejrgn@gmail.com>
2012-12-04 08:08:42 +0900

    얼왕 하드 던젼 열린것 수정 단 차후에 공장만 왕쓰자가 아닌 왕쓰자만 입던이 가능하게 될 수도 있음

--- src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
@@ -979,41 +979,6 @@ class instance_icecrown_citadel : public InstanceMapScript
                 if (player && AccountMgr::IsGMAccount(player->GetSession()->GetSecurity()))
                     return true;
 
-                // In heroic mode, leader must have NH Lich King achievement
-                bool heroicCheckFailed = true;
-                if (instance->IsHeroic())
-                {
-                    if (!player && instance->GetPlayersCountExceptGMs() > 0)
-                    {
-                        Map::PlayerList const& playerList = instance->GetPlayers();
-
-                        if (!playerList.isEmpty())
-                        {
-                            for (Map::PlayerList::const_iterator i = playerList.begin(); i != playerList.end(); ++i)
-                            {
-                                if (i->getSource())
-                                {
-                                    if (!i->getSource()->isGameMaster())
-                                    {
-                                        player = i->getSource();
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    if (player)
-                        if (const Group* group = player->GetGroup())
-                            if (group->GetLeaderGUID() && group->isRaidGroup())
-                                if (Player* leader = ObjectAccessor::GetPlayer(*player, group->GetLeaderGUID()))
-                                    if (leader->GetInstanceId() == instance->GetInstanceId())
-                                        if (leader->HasAchieved(instance->Is25ManRaid() ? 4597 : 4530))
-                                            heroicCheckFailed = false;
-                }
-                else
-                    heroicCheckFailed = false;
-
                 switch (bossId)
                 {
                     case DATA_THE_LICH_KING:
@@ -1047,7 +1012,7 @@ class instance_icecrown_citadel : public InstanceMapScript
                 if (!CheckLowerSpire(bossId))
                     return false;
 
-                return !heroicCheckFailed;
+                return true;
             }
 
             bool CheckPlagueworks(uint32 bossId) const
