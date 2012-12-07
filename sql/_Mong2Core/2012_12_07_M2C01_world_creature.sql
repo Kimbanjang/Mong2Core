410640a51310c2eca83ef7c248007346082c54c8
Kimbanjang <dhejrgn@gmail.com>
2012-12-07 21:29:32 +0900

    구린속, 썩은얼굴 버그 플레이로 인한 자리 이동

---
+++ sql/_Mong2Core/2012_12_07_M2C01_world_creature.sql
@@ -0,0 +1,3 @@
+-- 구린속/썩은얼굴 위치 이동 (흑마 소환으로 센터 뚫은 후 상층에서 때려 잡고 있음.. 하여간...)
+UPDATE `creature` SET `position_x`=4487.673340,`position_y`=3118.182861,`position_z`=360.60,`orientation`=2.596734 WHERE `id`=36627;
+UPDATE `creature` SET `position_x`=4228.640326,`position_y`=3116.546875,`position_z`=360.60,`orientation`=0.461239 WHERE `id`=36626;
