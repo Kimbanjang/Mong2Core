7454192d207837ef9e38ee3790f252d76abe5845
Kimbanjang <dhejrgn@gmail.com>
2012-12-08 09:38:44 +0900

    겨수 길목졸 이뮨 세팅

---
+++ sql/_Mong2Core/2012_12_08_M2C01_world_creature_template.sql
@@ -0,0 +1,3 @@
+-- 교수 중간몹 stinky / precious 이뮨설정 (중간보스)
+-- 반발시 8388624 + 스턴으로 교체 (차이가 있을지는 모르겠음)
+UPDATE `creature_template` SET `mechanic_immune_mask`=617298815 WHERE `entry` IN (37217,38103,37025,38064);
